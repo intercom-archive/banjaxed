0666b20b651d47a67c32d2784cff9b5717999eed
Eugene Kenny <eugene@intercom.io>
2014-07-30 23:01:33 +0100

    Comment model and migration
    Comments aren't owned by anyone yet, since users are authenticated but
    not persisted to the database. Comment ownership will be added later.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728132300) do
+ActiveRecord::Schema.define(version: 20140730214654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "incident_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "incidents", force: true do |t|
     t.string   "title"

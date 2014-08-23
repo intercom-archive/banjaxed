283c99058265d23839b8a1cbc54c5dc2ea3d59a3
Eugene Kenny <eugene@intercom.io>
2014-08-23 22:51:54 +0100

    Fix whitespace

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsersHelper, type: :helper do
   describe '#user_gravatar' do
-    let(:user) { FactoryGirl.create(:user, gravatar_hash: "some_hash")}
+    let(:user) { FactoryGirl.create(:user, gravatar_hash: "some_hash") }
     it "outputs the user's gravatar in the right size" do
       gravatar_html = helper.user_gravatar(user, 50)
       expect(gravatar_html).to match(%r{src="//gravatar.com/avatar/some_hash\?s=50&amp;d=mm"})

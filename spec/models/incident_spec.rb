dc8074cf098115cdb52e0887164a233c038f3b58
Eugene Kenny <eugene@intercom.io>
2014-11-07 13:41:11 +0000

    Fix deprecation warning from shoulda-matchers
    The ensure_inclusion_of matcher was renamed to validate_inclusion_of.

--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Incident, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
-  it { should ensure_inclusion_of(:severity).in_array(%w(critical high medium low)) }
-  it { should ensure_inclusion_of(:status).in_array(%w(open mitigated resolved closed)) }
+  it { should validate_inclusion_of(:severity).in_array(%w(critical high medium low)) }
+  it { should validate_inclusion_of(:status).in_array(%w(open mitigated resolved closed)) }
   it { should have_many(:comments) }
   it { should belong_to(:user) }
 end
